44770c34e521479dd7ca1d135704d4ba016a9498
hafsa chenni <hafsachenni77@gmail.com>
2023-08-21 02:57:51 -0700

    me

--- _printf.c
+++ _printf.c
@@ -1,4 +1,14 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * _printf - print function
+ * @format: format
+ * Return: chars
+ */
 
 int _printf(const char *format, ...)
 {
@@ -10,9 +20,9 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 
 	if (format == NULL)
-		return(-1);
+		return (-1);
 
-	while(*format)
+	while (*format)
 	{
 
 		if (*format == '%')
@@ -23,17 +33,17 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				break;
 
-			else if ( *format == 'c')
+			else if (*format == 'c')
 			{
 				c = va_arg(args, int);
-				write(1, $c, 1);
+				write(1, &c, 1);
 				index++;
 
 			}
 			else if (*format == 's')
 			{
-				str = va_arg(argc, char *);
-				write(1, format, strlen(str));
+				str = va_arg(args, char *);
+				write(1, str, strlen(str));
 				index += strlen(str);
 			}
 			else if (*format == '%')
