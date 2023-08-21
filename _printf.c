517b6ddf144185558cd43440e89b512257f00034
hafsa chenni <hafsachenni77@gmail.com>
2023-08-21 07:31:12 -0700

    me

--- _printf.c
+++ _printf.c
@@ -2,7 +2,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * _printf - print function
@@ -12,52 +13,58 @@
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list arg_p;
+	va_list args;
+	int index = 0;
+	char c;
+	int d;
+	char *str = NULL;
 
-
-	va_start(arg_p, format);
+	va_start(args, format);
 
 	if (format == NULL)
-	{
-		va_end(arg_p);
 		return (-1);
-	}
-
 
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
+
 			if (*format == '\0')
 				break;
-		if (*format == 'c')
-		{
-			char c = va_arg(arg_p, int);
-			write(1, &c, 1);
-			count++;
-		}
-		else if (*format == 's')
-		{
-			char *str = va_arg(arg_p, char *);
 
-			if (str != NULL)
+			else if (*format == 'c')
 			{
-				write(1, str, strlen(str));
-				count += strlen(str);
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				index++;
 			}
-		}
-		}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				write(1, str, strlen(str));
+				index += strlen(str);
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				d = va_arg(args, int);
+				sprintf(str, "%d", d);
+				write(1, str, strlen(str));
+				index += strlen(str);
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				index++;
+			}
+			}
 		else
 		{
 			write(1, format, 1);
-			count++;
+			index++;
 		}
 		format++;
 	}
-
-	va_end(arg_p);
-
-	return (count);
+	va_end(args);
+	return (index);
 }
